f7e091eebcf55c505c495fa5f5798d0287347874
lamnaourm <mohammed.lamnaour@gmail.com>
2022-12-26 15:02:02 +0100

    route 2

--- index.js
+++ index.js
@@ -20,6 +20,20 @@ app.post('/produit', (req, res) => {
     res.sendStatus(201);
 });
 
+app.get('/produit/all', (req, res) => {
+
+    var prds = []
+    
+    const files = fs.readdirSync(directory); 
+
+    files.forEach(file => {
+        const data = fs.readFileSync(`${directory}/${file}`,'utf8');
+        prds.push(JSON.parse(data))
+    });
+
+    res.status(202).json(prds);
+})
+
 app.listen(port, () => {
     console.log('Serveur lance ...');
 })
