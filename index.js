e8bed59d3152a0c809554b963d40a23b08fb1e7c
lamnaourm <mohammed.lamnaour@gmail.com>
2022-12-27 09:03:59 +0100

    use of path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { v4: uuid } = require("uuid");
+const path = require("path");
 const fs = require("fs");
 
 const app = express();
@@ -14,7 +15,8 @@ app.post("/produits", (req, res) => {
   }
 
   const id = uuid();
-  fs.writeFileSync(`${directory}/${id}.txt`, JSON.stringify(req.body));
+  //fs.writeFileSync(`${directory}/${id}.txt`, JSON.stringify(req.body));
+  fs.writeFileSync(path.join(directory, `${id}.txt`) , JSON.stringify(req.body));
   res.sendStatus(201);
 });
 
@@ -24,7 +26,8 @@ app.get("/produits/all", (req, res) => {
   const files = fs.readdirSync(directory);
 
   files.forEach((file) => {
-    const data = fs.readFileSync(`${directory}/${file}`, "utf8");
+    //const data = fs.readFileSync(`${directory}/${file}`, "utf8");
+    const data = fs.readFileSync(path.join(directory, file), "utf8");
     prds.push({ id: file.split(".")[0], ...JSON.parse(data) });
   });
 
@@ -34,11 +37,12 @@ app.get("/produits/all", (req, res) => {
 app.get("/produits/id/:id", (req, res) => {
   const id = req.params.id;
 
-  if (!fs.existsSync(`${directory}/${id}.txt`)) {
+  //if (!fs.existsSync(`${directory}/${id}.txt`)) {
+  if (!fs.existsSync(path.join(directory, `${id}.txt`))) {
     return res.sendStatus(404);
   }
 
-  const data = fs.readFileSync(`${directory}/${id}.txt`, "utf8");
+  const data = fs.readFileSync(path.join(directory, `${id}.txt`), "utf8");
   res.json({ id: id, ...JSON.parse(data) });
 });
 
@@ -49,7 +53,7 @@ app.get("/produits/famille/:famille", (req, res) => {
   const files = fs.readdirSync(directory);
 
   files.forEach((file) => {
-    const data = fs.readFileSync(`${directory}/${file}`, "utf8");
+    const data = fs.readFileSync(path.join(directory, file), "utf8");
     const produit = JSON.parse(data);
 
     if (produit.famille === famille)
@@ -66,14 +70,14 @@ app.put("/produits/:id", (req, res) => {
     return res.sendStatus(404);
   }
 
-  fs.writeFileSync(`${directory}/${id}.txt`, JSON.stringify(req.body));
+  fs.writeFileSync(path.join(directory, `${id}.txt`), JSON.stringify(req.body));
   res.end();
 });
 
 app.delete("/produits/:id", (req, res) => {
     const id = req.params.id;
 
-    if (!fs.existsSync(`${directory}/${id}.txt`)) {
+    if (!fs.existsSync(path.join(directory, `${id}.txt`))) {
       return res.sendStatus(404);
     }
 
